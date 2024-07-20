356c1ec5fca6f7fadc5a8d1897e335a1d097e000
quantfreedom <quantfreedom1022@gmail.com>
2024-07-20 11:50:12 -0700

    update

--- z/asdf string formating.py
+++
@@ -1,7 +0,0 @@
-number = 16.634
-message = f"Number is {number:.2f}"
-print(message)
-for num in range(1, 11):
-    print(f"the number is {num:.2f}")
-
-# https://www.w3schools.com/python/ref_string_format.asp
